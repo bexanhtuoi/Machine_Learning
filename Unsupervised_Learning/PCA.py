e1ec35a89a35547532d5e6753b198e271f3e92b2
Hoàng is my name <bexanhtuoi@gmail.com>
2025-03-01 09:39:45 +0700

    init

--- Unsupervised_Learning/PCA.py
+++ Unsupervised_Learning/PCA.py
@@ -1,5 +1,6 @@
 def PCA(X, dimension: int):
   n, d = X.shape
+  #Chuẩn hóa dữ liệu
   _mean = []
   _std = []
   for i in X.T:
@@ -7,8 +8,13 @@ def PCA(X, dimension: int):
     _std.append(np.std(i))
   X = (X - _mean) / _std
 
+  #Tính ma trận hiệp phương sai
+  _mean = [np.mean(i) for i in X.T]
+
   X = X - _mean
   cov = 1/n * X.T @ X
+
+  #Tính trị riêng và vector riêng
   eigenvalues, eigenvectors = np.linalg.eig(cov)
 
   return X @ eigenvectors[:, :dimension]
